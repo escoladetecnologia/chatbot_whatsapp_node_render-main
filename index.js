112de1edc5ff9518daa6eabbec9c06e1b7231d49
Claudio M. Antonio <96396925+escoladetecnologia@users.noreply.github.com>
2024-11-02 00:30:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Importando bibliotecas
+import express from 'express';
 import qrcode from 'qrcode';
 import qrcodeTerminal from 'qrcode-terminal';
 import { Client } from 'whatsapp-web.js';
-import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-// Variáveis de ambiente para conexão com o MongoDB
-const uri = process.env.MONGODB_URI; // Certifique-se de definir esta variável de ambiente no Render
+// Configurações do MongoDB
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+    throw new Error("A variável de ambiente MONGODB_URI não está definida.");
+}
+
+// Cria um cliente do MongoDB
 const clientMongo = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -15,16 +21,7 @@ const clientMongo = new MongoClient(uri, {
     }
 });
 
-// Inicializando o cliente do WhatsApp
-const app = express();
-let latestQrCodeUrl = ''; // Variável para armazenar o QR code atualizado
-const whatsappClient = new Client({
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-    }
-});
-
-// Conexão com o MongoDB
+// Função para conectar ao MongoDB
 async function connectToMongoDB() {
     try {
         await clientMongo.connect();
@@ -34,25 +31,32 @@ async function connectToMongoDB() {
         console.error("Erro ao conectar ao MongoDB:", error);
     }
 }
-connectToMongoDB();
 
-// Opção de exibir QR Code no terminal e no navegador
-whatsappClient.on('qr', async qr => {
-    qrcodeTerminal.generate(qr, { small: true });
-    
-    try {
-        latestQrCodeUrl = await qrcode.toDataURL(qr, {
-            errorCorrectionLevel: 'H',
-            width: 300,
-            margin: 2
-        });
-        console.log("QR Code atualizado e disponível em: https://chatbot-whatsapp-node-render.onrender.com/qrcode");
-    } catch (error) {
-        console.error("Erro ao gerar QR Code:", error);
+// Inicializando o cliente do WhatsApp com configurações para WhatsApp Web
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'] // Argumentos para contornar restrições
     }
 });
 
+let latestQrCodeUrl = ''; // Variável para armazenar o QR code atualizado
+
+// Opção de exibir QR Code no terminal e no navegador
+client.on('qr', async qr => {
+    // Exibindo QR Code no terminal com a opção "small"
+    qrcodeTerminal.generate(qr, { small: true });
+    
+    // Atualiza o QR code a cada vez que há um novo evento `qr`
+    latestQrCodeUrl = await qrcode.toDataURL(qr, {
+        errorCorrectionLevel: 'H',
+        width: 300,
+        margin: 2
+    });
+    console.log("QR Code atualizado e disponível em: https://chatbot-whatsapp-node-render.onrender.com/qrcode");
+});
+
 // Rota para exibir o QR Code atualizado no navegador
+const app = express();
 app.get('/qrcode', (req, res) => {
     if (latestQrCodeUrl) {
         res.send(`
@@ -73,55 +77,66 @@ app.listen(port, () => {
 });
 
 // Evento quando o cliente está pronto
-whatsappClient.on('ready', () => {
+client.on('ready', () => {
     console.log('Tudo certo! WhatsApp conectado.');
 });
 
 // Inicializa o cliente
-whatsappClient.initialize();
+client.initialize();
 
 // Função para criar um delay entre as ações
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 // Configurações de mensagens automáticas
-whatsappClient.on('message', async msg => {
+client.on('message', async msg => {
     if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola|quero|Quero|mais|informacao|informação|informações|saber|curso|matricula|Matricula|matrícula|Matrícula|comprar|comprar|duvida|dúvida|Duvida|Dúvida|Ajuda|ajuda|falar|Falar)/i) && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
         await delay(3000);
         await chat.sendStateTyping();
         await delay(3000);
         const contact = await msg.getContact();
-        const name = contact.pushname || "usuário";
-        await whatsappClient.sendMessage(msg.from, `Olá, ${name.split(" ")[0]}! Sou o assistente virtual da empresa Escola de Tecnologia. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Mais Informação - Curso Cypecad\n2 - Curso Cype 3D Metálicas\n3 - Outras perguntas`);
+        const name = contact.pushname;
+        await client.sendMessage(msg.from, `Olá, ${name.split(" ")[0]}! Sou o assistente virtual da empresa Escola de Tecnologia. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Mais Informação - Curso Cypecad\n2 - Curso Cype 3D Metálicas\n3 - Outras perguntas`);
     }
 
-    // Resposta para o curso Cypecad
     if (msg.body === '1' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
         await delay(3000);
         await chat.sendStateTyping();
         await delay(3000);
-        await whatsappClient.sendMessage(msg.from, `Aqui estão as informações sobre o Curso Cypecad:\n\nEnfrentar qualquer cálculo estrutural com Ajuda de Um Engenheiro Calculista Especialista...`);
-        // Continue com a mensagem do curso...
+        await client.sendMessage(msg.from, `Aqui estão as informações sobre o Curso Cypecad:\n\nEnfrentar qualquer cálculo estrutural com Ajuda de Um Engenheiro Calculista Especialista e faturar de R$ 15.000 a R$ 50.000 ou mais com projetos de edifícios, sobrados, casas de acordo com a NBR6118 através do nosso método único VQS (Velocidade, Qualidade, Segurança), indo além de ser Piloto de Software.\n\nCalcular, Detalhar, Projetar e Analisar um projeto completo de concreto armado.\n\nDar um UP na sua Carreira, e obter os melhores empregos, salários e negócios.\n\nFazer Detalhamento de Vigas, Pilares, Lajes, Fundações.\n\nFazer cálculo de Fundações conforme NBR6122.\n\nFazer os Carregamentos conforme normas NBR 6120, barras NBR 7480, ventos NBR 6123, ações e combinações.`);
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Curso Cypecad na Prática - Cálculo Estrutural: 12x R$ 34,90 ou R$ 349,00 à vista. Assim que o sistema confirmar o pagamento, você receberá os dados de acesso ao curso');
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Você pode fazer a compra do curso através deste link: https://sun.eduzz.com/wcs7e6ps');
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Te Vejo lá na Plataforma do Curso Cypecad 😊'); 
     }
 
-    // Resposta para o curso Cype 3D Metálicas
     if (msg.body === '2' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
         await delay(3000);
         await chat.sendStateTyping();
         await delay(3000);
-        await whatsappClient.sendMessage(msg.from, `Aqui estão as informações sobre o Curso Cype 3D Metálicas:\n\nDomine o Cype 3D Estruturas Metálicas em VideoAulas Passo a Passo...`);
-        // Continue com a mensagem do curso...
+        await client.sendMessage(msg.from, `Aqui estão as informações sobre o Curso Cype 3D Metálicas:\n\nDomine o Cype 3D Estruturas Metálicas em VideoAulas Passo a Passo e Seja um Especialista em Cálculo Estrutural de Galpões Metálicos!\n\n Magno Moreira, Engenheiro de Elite, Revela o Método VQS para fazer Projetos de Estruturas Metálicas com mais Velocidade, Qualidade e Segurança.\n\nO Curso Cype 3D Estruturas Metálicas ensina na prática um projeto real de Galpão Metálico de 640m2 e Mezanino como calcular e dimensionar o projetos de estruturas metálicas de acordo com as normas brasileiras ((NBR 6120), barras (NBR 7480), ventos (NBR 6123), ações e combinações)`);
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Curso Cype 3D Metálicas na Prática - Cálculo Estrutural de Galpões Metálicos: 12x R$ 34,90 ou R$ 349,00 à vista. Assim que o sistema confirmar o pagamento, você receberá os dados de acesso ao curso');
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Você pode fazer a compra do curso através deste link: https://sun.eduzz.com/7czxg5un');
+        await delay(1000);
+        await client.sendMessage(msg.from, 'Te Vejo lá na Plataforma do Curso Cype 3D Metálicas 😊'); 
     }
 
-    // Resposta para outras perguntas
     if (msg.body === '3' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
         await delay(3000);
         await chat.sendStateTyping();
         await delay(3000);
-        await whatsappClient.sendMessage(msg.from, 'Se você tiver outras dúvidas ou precisar de mais informações, por favor, digite sua pergunta abaixo e aguarde a resposta.');
-        // Continue com a mensagem de espera...
+        await client.sendMessage(msg.from, 'Se você tiver outras dúvidas ou precisar de mais informações, por favor, digite sua pergunta abaixo e aguarde a resposta');
+        await delay(3000);
+        await client.sendMessage(msg.from, 'Aguarde que um de nossos Atendentes irá responder a sua dúvida, caso queira conhecer todos os nossos cursos acesse o site: https://www.escoladetecnologia.com');
     }
 });
+
+// Conectar ao MongoDB ao iniciar o aplicativo
+connectToMongoDB().catch(console.error);
